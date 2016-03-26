3005b1de3b5b3d0016efb269c7cd2e7cfb4fd991
Mikhail Aleynikov <bobro-krab@bk.ru>
2016-03-26 16:51:29 +0600

    Implemented testing.

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -15,29 +15,19 @@ enum some { array_size = 315 }; // size is pi(3.1415 * 100)
 float random_standart();
 float random_distributed();
 void test_distributed(int item_count);
+void test_with_returns();
+void test_without_returns();
 
 int main(int argc, char **argv)
 {
-    int item_count = 10000;
+    int item_count = 100000;
     if (argc > 1) {
         item_count = atoi(argv[1]);
     }
 
-    // vector<pair<int, float> > distribution;
-    // class random_with_returns returned(distribution);
-    class random_without_returns returned("input_without_returns");
-    returned.print_distribution();
-
-    try {
-        for (int i = 0; i < 10; ++i){
-            // cout << returned.random()<<endl;
-            cout << returned.random() << "\n";
-            // cout << random_int(100) << endl;
-        }
-    } catch (string s) {
-        cout << s << endl;
-    }
-
+    test_distributed(item_count);
+    test_without_returns();
+    test_with_returns();
 
     return 0;
 }
@@ -62,11 +52,47 @@ float random_distributed()
 void test_distributed(int item_count)
 {
     int histogram[array_size] = {0};
+    ofstream out;
+    out.open("distributed.res");
+    cout << "Distributed save to 'Distributed.res' file" << endl;
     for (int i = 0; i < item_count; ++i){
         histogram[(int)(random_distributed() * 100)] += 1;
     }
 
     for (int i = 0; i < array_size; ++i) {
-        cout << histogram[i] << endl;
+        out << histogram[i] << endl;
+    }
+    out.close();
+}
+
+void test_without_returns()
+{
+    class random_without_returns returned("input_without_returns");
+    returned.print_distribution();
+
+    cout << "without Returns result" << endl;
+    try {
+        for (int i = 0; i < 10; ++i){
+            cout << returned.random() << "\n";
+        }
+    } catch (string s) {
+        cout << s << endl;
+    }
+
+}
+
+void test_with_returns()
+{
+    class random_with_returns returned("input");
+    returned.print_distribution();
+    int statistics[4] = {0};
+
+    for (int i = 0; i < 10000; ++i){
+        statistics[returned.random()]++;
+    }
+
+    cout << "With returns histogram" << endl;
+    for (int i = 0; i < 3; ++i){
+        cout << statistics[i+1] << endl;
     }
 }
