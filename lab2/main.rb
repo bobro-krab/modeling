f431efb564ec3cf8302e67fa18321e4c78077000
bobro-krab <bobro-krab@bk.ru>
2016-04-02 12:17:08 +0600

    Enabled matrix print.

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,9 +1,19 @@
-verticle_count = 20
+verticle_count = 10
 max_degeee = 5
 
-verticles =%w[]
+vericle_degrees =%w[]
 1.upto(verticle_count) do |e|
-    verticles.insert(0,  Random.rand(max_degeee))
-    p verticles[0]
+    vericle_degrees.insert(0,  Random.rand(max_degeee) + 1)
 end
-p verticles.size
+p "degrees:"
+p  vericle_degrees
+
+matrix = Array.new(verticle_count ).map!{Array.new(verticle_count)}
+
+p "matrix:"
+(0..verticle_count - 1).each do |i|
+    (0..verticle_count - 1).each do |j|
+        matrix[i][j] = 0
+    end
+    p matrix[i]
+end
