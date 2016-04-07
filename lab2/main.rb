9a7c26acbd4d8dc1085607757fb4a6f03231ff84
Mikhail Aleynikov <bobro-krab@bk.ru>
2016-04-07 18:14:10 +0600

    Implemented randomGraph function.

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,22 +1,3 @@
-verticle_count = 10
-max_degeee = 5
-
-list = Array.new(verticle_count).map!{Array.new{1}}
-
-def printGraph list
-    p "list"
-    verticle_index = 1
-    list.each do |l|
-        print(verticle_index, " ", l, "\n")
-        verticle_index+=1
-    end
-end
-
-def addVerticlesToGraph(list, verticle, verticle2)
-    list[verticle][list[verticle].size]=verticle2
-    list[verticle2][list[verticle2].size]=verticle
-end
-
 
 class Pair
     attr_accessor :verticle
@@ -43,37 +24,37 @@ def traverse current, parent, depth = 0
 end
 
 
-unused_verticles = %w[]
-used_verticles = %w[]
-1.upto(verticle_count) do |e|
-    unused_verticles.push(Pair.new(e, Random.rand(max_degeee) +1))
+def randomGraph verticle_count, max_degeee
+    unused_verticles = %w[]
+    used_verticles = %w[]
+    1.upto(verticle_count) do |e|
+        unused_verticles.push(Pair.new(e, Random.rand(max_degeee) +1))
+    end
+
+    # select random verticle
+    verticle = unused_verticles[Random.rand(unused_verticles.size)]
+    unused_verticles.delete(verticle)
+    used_verticles.push(verticle)
+    start = verticle
+
+    # add to allowed its neighbor/
+    while unused_verticles.size > 0
+        # select random verticle from allowed
+        verticle     = used_verticles[Random.rand(used_verticles.size)]
+        new_verticle = unused_verticles[Random.rand(unused_verticles.size)]
+
+        # move new verticle to used
+        unused_verticles.delete(new_verticle)
+        used_verticles.push(new_verticle)
+
+        # link verticles
+        if verticle != nil; verticle.linked_with.push(new_verticle) end
+        new_verticle.linked_with.push(verticle)
+    end
+    return start
 end
-# p "degrees:"
-# p unused_verticles
 
-# select random verticle
-verticle = unused_verticles[Random.rand(unused_verticles.size)]
-start = verticle
-unused_verticles.delete(verticle)
-used_verticles.push(verticle)
-p unused_verticles
-
-# add to allowed its neighbor/
-while unused_verticles.size > 0
-    # select random verticle from allowed
-    new_verticle = unused_verticles[Random.rand(unused_verticles.size)]
-
-    # add it into the graph
-    unused_verticles.delete(new_verticle)
-    verticle.linked_with.push(new_verticle)
-    new_verticle.linked_with.push(verticle)
-
-    # remove merged verticles from allowed
-    # add newly linked verticles to allowed
-    verticle = new_verticle
-end
-
-traverse start, nil, 1
+traverse randomGraph(10000, 5), nil
 
 # printGraph list
 
