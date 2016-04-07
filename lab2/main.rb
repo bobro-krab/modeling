a1a6f2545a0ea56a1bfd31ada03bed0a850ab545
bobro-krab <bobro-krab@bk.ru>
2016-04-07 11:12:02 +0600

    Implemented linking between two verticles.

--- lab2/main.rb
+++ lab2/main.rb
@@ -3,11 +3,6 @@ max_degeee = 5
 
 list = Array.new(verticle_count).map!{Array.new{1}}
 
-def addVerticlesToGraph(list, verticle, verticle2)
-    list[verticle][list[verticle].size]=verticle2
-    list[verticle2][list[verticle2].size]=verticle
-end
-
 def printGraph list
     p "list"
     verticle_index = 1
@@ -17,19 +12,48 @@ def printGraph list
     end
 end
 
-vericle_degrees = {}
+def addVerticlesToGraph(list, verticle, verticle2)
+    list[verticle][list[verticle].size]=verticle2
+    list[verticle2][list[verticle2].size]=verticle
+end
+
+
+class Pair
+    attr_accessor :verticle
+    attr_accessor :degree
+    attr_accessor :linked_with
+    def initialize (a,b)
+        @verticle = a
+        @degree = b
+        @linked_with = %w[]
+    end
+end
+
+verticle_degrees = %w[]
 1.upto(verticle_count) do |e|
-    vericle_degrees[e] =  Random.rand(max_degeee) + 1
+    verticle_degrees.push(Pair.new(e, Random.rand(max_degeee) +1))
 end
 p "degrees:"
-p  vericle_degrees
+p verticle_degrees
+
+allowed_verticles = %w[]
 
 # select random verticle
-# add to allowed its neighborg
+verticle = verticle_degrees[Random.rand(verticle_degrees.size)]
+verticle_degrees.delete(verticle)
+p verticle_degrees
+# add to allowed its neighbor/
 
 # select random verticle from allowed
+new_verticle = verticle_degrees[Random.rand(verticle_degrees.size)]
+verticle_degrees.delete(new_verticle)
+verticle.linked_with.push(new_verticle)
+new_verticle.linked_with.push(verticle)
+p verticle
+p new_verticle
+# add it into the graph
 # remove merged verticles from allowed
 # add newly linked verticles to allowed
 
-printGraph list
+# printGraph list
 
