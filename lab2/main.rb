5bb22fea52a0bec656b507b0706e1771bf90f440
Mikhail Aleynikov <bobro-krab@bk.ru>
2016-04-09 16:11:55 +0600

    Max depth graph implemented

--- lab2/main.rb
+++ lab2/main.rb
@@ -2,32 +2,70 @@
 class Pair
     attr_accessor :verticle
     attr_accessor :degree
+    attr_accessor :depth
     attr_accessor :linked_with
     def initialize (a,b)
         @verticle = a
         @degree = b
+        @depth = 0
         @linked_with = %w[]
     end
 end
 
-def traverse current, parent = nil, depth = 0
+
+
+def traverse currentNode, parent = nil, depth = 0
     1.upto(depth) do
         printf "  "
     end
-    puts current.verticle
-    current.linked_with.each do |sibling|
+    puts currentNode.verticle
+    currentNode.linked_with.each do |sibling|
         if sibling == parent
         else
-            traverse sibling, current, depth + 1
+            traverse sibling, currentNode, depth + 1
         end
     end
 end
 
+
+
 def graphWithMaxDepth verticle_count, max_depth
-    unused_verticles = %w[]
+    unused_verticles   = %w[] # which not already added to graph
+    internal_verticles = %w[] # verticles can be attached to
+    external_verticles = %w[] # which reached max depth
+    1.upto(verticle_count) do |e|
+        unused_verticles.push(Pair.new(e, 0))
+    end
+
+    # select root
+    root = unused_verticles[Random.rand(unused_verticles.size)]
+    unused_verticles.delete(root)
+    internal_verticles.push(root)
+
+    while unused_verticles.size > 0
+        verticle = internal_verticles[Random.rand(internal_verticles.size)]
+        new_verticle = unused_verticles[Random.rand(unused_verticles.size)]
+
+        # link verticles
+        verticle.linked_with.push(new_verticle)
+        new_verticle.linked_with.push(verticle)
+
+        # place on edge or on internal plane
+        new_verticle.depth = verticle.depth + 1
+        if new_verticle.depth == max_depth
+            external_verticles.push(new_verticle)
+        else
+            internal_verticles.push(new_verticle)
+        end
+        unused_verticles.delete(new_verticle)
+    end
+
+    return root
 end
 
-def randomGraph verticle_count, max_degeee
+
+
+def graphWithMaxDegree verticle_count, max_degeee
     unused_verticles = %w[]
     used_verticles = %w[]
     useless_verticles = %w[]
@@ -58,7 +96,6 @@ def randomGraph verticle_count, max_degeee
             unused_verticles.delete(verticle)
         end
 
-
         # move new verticle to used
         unused_verticles.delete(new_verticle)
         used_verticles.push(new_verticle)
@@ -70,4 +107,7 @@ def randomGraph verticle_count, max_degeee
     return start
 end
 
-traverse randomGraph(10, 3)
+puts "degree"
+traverse graphWithMaxDegree(10, 2)
+puts "depth"
+traverse graphWithMaxDepth(10, 2)
