1946db452047ac1df16488c32d87f95515e8c30a
bobro-krab <bobro-krab@bk.ru>
2016-05-24 13:19:05 +0600

    6 nearly done.

--- lab6/main.rb
+++ lab6/main.rb
@@ -1,42 +1,58 @@
 #!/usr/bin/env ruby
 
+StepsLimit = 150
+
 global_queue = []
 n = 0
+steps = 0
+
+def distirbution
+    return rand.round(4)
+end
 
 def schedule (event, queue)
     queue.push(event)
-    queue = queue.sort {|a, b|
+    queue.sort! {|a, b|
         a[:time] <=> b[:time]
     }
 end
 
 def simulate queue
-    p queue
-    queue.each do |key|
-        print "trying to call "
-        p key
+    while queue.size > 0 do
+        key = queue.shift
+        p queue
+        # у каждой задачи уменьшаем время оставшееся в очереди
+        queue.each { |element| element[:time] -= key[:time] }
+        print " minus #{ key[:time]}\n"
+        p queue
         key[:call].call(queue)
+        queue.delete(key)
     end
 end
 
 E2 = Proc.new { |queue|
+    if steps >= StepsLimit
+        next
+    end
+    steps += 1
     n -= 1
     print "E2 n is #{n}\n"
-    if n < 3
-        exit
-    end
-    schedule({:time => rand.round(2), :call => E2}, queue)
+    schedule({:time => distirbution, :call => E2}, queue)
 }
 
 E1 = Proc.new do |queue|
-    n += 1
-    if n > 3
-        exit
+    if steps >= StepsLimit
+        next
     end
+    steps += 1
+    n += 1
     print "E1 n is #{n} \n"
-    schedule({:time => rand.round(2), :call => E1}, queue)
+    schedule({:time => distirbution, :call => E1}, queue)
 end
 
-schedule({:time => rand.round(2), :call => E1}, global_queue)
-schedule({:time => rand.round(2), :call => E2}, global_queue)
+schedule({:time => distirbution, :call => E2}, global_queue)
+schedule({:time => distirbution, :call => E1}, global_queue)
+p global_queue
+
 simulate(global_queue)
+print "Steps is #{steps}"
